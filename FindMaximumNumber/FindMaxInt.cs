888bc5ed7f4f30a4bbd07205549d242ca538065b
akshata <akshata2018p@gamil.com>
2023-02-21 07:46:54 +0530

    [akshata]Add:Find Maximum Number

---
+++ FindMaximumNumber/FindMaxInt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class FindIntMax
+    {
+
+        public void FindMaxValue(int First, int Second, int Third)       //UC1 finding max of 3 numbers.
+        {
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
+            {
+                Console.WriteLine("First number is Greater :" + First);
+            }
+            else if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
+            {
+                Console.WriteLine("Second number is Greater :" + Second);
+            }
+            else if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
+            {
+                Console.WriteLine("Third number is Greater :" + Third);
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+    }
+}
---
+++ FindMaximumNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using FindMaximumNumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FindIntMax integerfindmax = new FindIntMax();
+            integerfindmax.FindMaxValue(13,20,23);
+            Console.ReadLine();
+        }
+    }
+}

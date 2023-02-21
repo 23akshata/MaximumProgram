57eecb31d793d5602bddb0d9ec2ab30fb2bb6a23
akshata <akshata2018p@gamil.com>
2023-02-21 08:05:48 +0530

    [akshata]Add:Find Maximum Float

--- FindMaximumNumber/FindMaxInt.cs
+++ FindMaximumNumber/FindMaxInt.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FindMaximumNumber
+namespace FindMaximum
 {
     public class FindIntMax
     {
@@ -13,18 +13,36 @@ namespace FindMaximumNumber
         {
             if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
             {
-                Console.WriteLine("First number is Greater :" + First);
+                Console.WriteLine("first is greater :" + First);
             }
             else if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
             {
-                Console.WriteLine("Second number is Greater :" + Second);
+                Console.WriteLine("second is greater :" + Second);
             }
             else if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
             {
-                Console.WriteLine("Third number is Greater :" + Third);
+                Console.WriteLine("third is greater :" + Third);
             }
             else
                 Console.WriteLine("All Numbers are Equal! ");
+        }
+        public void FindMaxValue(float First, float Second, float Third)    // UC2 Finding maximum Of Three Float
+        {
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
+            {
+                Console.WriteLine("first is greater :" +First);
+            }
+            else if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
+            {
+                Console.WriteLine("second is greater :" + Second);
+            }
+            else if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0)
+            {
+                Console.WriteLine("third is greater :" +Third);
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+
         }
     }
 }
--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -1,11 +1,11 @@
-﻿using FindMaximumNumber;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FindMaximumNumber
+namespace FindMaximum
 {
     public class Program
     {
@@ -13,6 +13,7 @@ namespace FindMaximumNumber
         {
             FindIntMax integerfindmax = new FindIntMax();
             integerfindmax.FindMaxValue(13,20,23);
+            integerfindmax.FindMaxValue(9.5f, 10.2f, 23.4f);
             Console.ReadLine();
         }
     }
